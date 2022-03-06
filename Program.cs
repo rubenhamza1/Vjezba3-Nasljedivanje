2c20c0a60bd2db98926ecde531a4b06b98c9d179
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-03-06 17:35:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biljka
+{
+    class Biljka
+    {
+
+    }
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+
+        public Stablo(bool otpadajuListovi)
+        {
+            this.otpadajuListovi = otpadajuListovi;
+        }
+
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+    }
+    class Cvijet : Biljka
+    {
+
+    }
+    class Crnogoricno : Stablo
+    {
+        
+    }
+    class Bijelogoricno : Stablo
+    {
+      
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
